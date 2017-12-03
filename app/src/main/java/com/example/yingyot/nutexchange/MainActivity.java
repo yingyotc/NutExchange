afae2b02630cc772f80d8c352be7fa4b86a16223
Yingyot <yingyotc@hotmail.com>
2017-12-03 10:33:04 +0700

    Add Fragment Success

--- app/src/main/java/com/example/yingyot/nutexchange/MainActivity.java
+++ app/src/main/java/com/example/yingyot/nutexchange/MainActivity.java
@@ -3,11 +3,25 @@ package com.example.yingyot.nutexchange;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.yingyot.nutexchange.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+//        Add Fragment to Activity
+        if (savedInstanceState == null) {
+//            Condition is True
+
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.contentMainFragment, new MainFragment())
+                    .commit();
+        }   // if
+
+    }   // Main Method
+
+}   // Main Class
