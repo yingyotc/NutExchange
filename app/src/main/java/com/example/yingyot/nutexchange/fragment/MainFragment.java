d92d3a327e3478be4f780ea12e9cecc8f3353785
Yingyot <yingyotc@hotmail.com>
2017-12-03 11:51:26 +0700

    Check on USD

--- app/src/main/java/com/example/yingyot/nutexchange/fragment/MainFragment.java
+++ app/src/main/java/com/example/yingyot/nutexchange/fragment/MainFragment.java
@@ -6,6 +6,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import com.example.yingyot.nutexchange.R;
 
@@ -15,6 +19,42 @@ import com.example.yingyot.nutexchange.R;
 
 public class MainFragment extends Fragment{
 
+    //    Explicit หรือ การประกาศตัวแปร
+    private RadioGroup radioGroup;
+    private RadioButton usdRadioButton, thbRadioButton;
+    private EditText editText;
+    private Button button;
+
+
+//    สร้างMainMethod ที่ใช้ในการจัดการ การทำงาน
+
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Initial View
+        initialView();
+
+//        Click USD to THB First
+        clickUSDToTHBFirst();
+
+
+    }   // Main Method
+
+    private void clickUSDToTHBFirst() {
+        usdRadioButton.setChecked(true);
+    }
+
+    private void initialView() {
+        radioGroup = getView().findViewById(R.id.ragMoney);
+        usdRadioButton = getView().findViewById(R.id.radUSD);
+        thbRadioButton = getView().findViewById(R.id.radTHB);
+        editText = getView().findViewById(R.id.edtMoney);
+        button = getView().findViewById(R.id.btnExchange);
+    }
+
+    //    สร้างหน้ากาก Fragment
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater,
